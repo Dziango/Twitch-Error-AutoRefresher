79ad040ac8736bac850fe1b33559b43332da3c95
Dziango <Konrad.Joniak@gmail.com>
2019-02-09 18:58:03 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,26 @@
+var observer = new MutationObserver(function(mutations, observer) {
+    FindError();
+});
+
+var player = 0;
+
+function FindError() {
+  if (player.innerHTML.indexOf('Error #2000') > -1) {
+    chrome.runtime.sendMessage({ text: "Please refresh" });
+  }
+}
+
+function WaitForPlayer() {
+  player = document.getElementById('default-player');
+  if (player == null) {
+    window.requestAnimationFrame(WaitForPlayer);
+  }
+  else {
+    observer.observe(player, {
+      childList: true,
+      subtree: true
+    });
+   }
+};
+
+WaitForPlayer()
