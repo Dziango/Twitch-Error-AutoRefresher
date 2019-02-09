dd9e76276673f3cc66288c72bd4c7f2821f62c7d
Dziango <Konrad.Joniak@gmail.com>
2019-02-10 00:10:57 +0100

    Fix (accidentialy deleted one crucial line)

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ var observer = new MutationObserver(function(mutations, observer) {
 var player = 0;
 
 function FindError() {
+  player = document.getElementById('default-player');
   if (player.innerHTML.indexOf('Error #2000') > -1) {
     chrome.runtime.sendMessage({ text: "Please refresh" });
   }
