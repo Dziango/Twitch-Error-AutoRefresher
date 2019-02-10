f650f9a25d12d33ace0360527fb9ae93fab99bc5
Dziango <Konrad.Joniak@gmail.com>
2019-02-10 14:04:13 +0100

    Initial commit

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.text == "Please refresh") {
-        chrome.tabs.reload(sender.tab.id);
-        sendResponse({ text: "You got it" });
-    }
-});
