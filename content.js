d9c5826736281fa996523c98b381124805ce39bd
Dziango <Konrad.Joniak@gmail.com>
2019-02-12 21:30:16 +0100

    Now works with every kind of error

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ var player = 0;
 
 function FindError() {
   player = document.getElementById('default-player');
-  if (player.innerHTML.indexOf('Error #2000') > -1) {
+  if (player.innerHTML.indexOf('Error') > -1) {
     chrome.runtime.sendMessage({ text: "Please refresh" });
   }
 }
